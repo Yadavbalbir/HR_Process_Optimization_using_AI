b061282ec782ddb74ae464727e9bd41ac4a8ca31
Balbir Yadav <balbir.iitbombay3602@gmail.com>
2023-11-27 20:54:11 +0530

    added fully functional code

--- main/views.py
+++ main/views.py
@@ -5,13 +5,13 @@ from hpo import settings
 import ast
 from openai import OpenAI
 client = OpenAI(
-    api_key=""
+    api_key="" #Enter your OpenAI API key
 )
 
 def index(request):
     return render(request, "index.html")
 
-def get_prompt(jd, job_title, resume):
+def get_resume_score_prompt(jd, job_title, resume):
     print("################################################################")
     print("Generating prompt...")
     print("################################################################")
@@ -37,6 +37,20 @@ def get_prompt(jd, job_title, resume):
 
     return prompt
 
+
+def get_question_for_candidate(job_title, jd, resume):
+    print("################################################################")
+    print("Generating questions for candidate...")
+    print("################################################################")
+    prompt = '''
+    job_title: {}
+    job_description: {}
+    condsider the given below resume and above job title and description. ask 1 question on skils related to job description, 2 questions on technical details of the projects mentioned in the resume, 2 questions on any thing.  
+    candidates_Resume_text: {}
+
+    '''.format(job_title, jd, resume)
+    return prompt
+
 def demo(request):
     if request.method == 'POST':
     # Process each uploaded file
@@ -47,14 +61,12 @@ def demo(request):
         for uploaded_file in request.FILES.getlist('resumes'):
             resume_content.append(handle_uploaded_file(uploaded_file))
         # Add your logic for resume processing here
-        # ...
         i = 1
         for resume in resume_content:
-            prompt = get_prompt(jd, job_title, resume)
+            prompt = get_resume_score_prompt(jd, job_title, resume)
             print("################################################################")
             print("Prompt Generated successfully for candidate "+ str(i))
-            i += 1
-            print("################################################################")
+            
 
             print("Now Using LLM for score calculation...")
             completion = client.chat.completions.create(
@@ -64,18 +76,44 @@ def demo(request):
                 {"role": "user", "content": prompt }
             ]
             )
-            print("################################################################")
             print("LLM response received successfully")
             message = completion.choices[0].message
             candidate = ast.literal_eval(message.content)
+
+            questions_prompt = get_question_for_candidate(job_title, jd, resume)
+            print("################################################################")
+
+            print("Now generating interview questions for candidate " + str(i))
+            completion = client.chat.completions.create(
+            model="gpt-3.5-turbo",
+            messages=[
+                {"role": "system", "content": "Evaluate resume for a given job position based on the given job description. generate 5 question. give the questions in json format where key will be number like 1, 2,3 and value be the question"},
+                {"role": "user", "content": questions_prompt }
+            ]
+            )
+            print("################################################################")
+            print("LLM response for question received successfully")
+            message = completion.choices[0].message
+            print(message.content)
+            questions = ast.literal_eval(message.content)
+            print(questions['1'])
+            candidate['q1'] = questions['1']
+            candidate['q2'] = questions['2']
+            candidate['q3'] = questions['3']
+            candidate['q4'] = questions['4']
+            candidate['q5'] = questions['5']
+            i += 1
             shortlisted.append(candidate)
-            print(candidate, type(candidate))
-        print(shortlisted)
+            print("################################")
+            print("Candidates resume score and questions generated successfully")
+
         context = {
             "shortlisted": shortlisted,
             "job_title": job_title,
             "jd": jd
         }
+
+        
         return render(request, "results.html", context)  # Redirect to a success page
     return render(request, "demo.html")
 
